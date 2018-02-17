e25ecad485afb0c61af45d563ff4eb7f78ef3e45
Rizal Muttaqin <riz_17_oke@yahoo.co.id>
2018-02-18 05:53:53 +0700

    Add script installer

---
+++ install-karasa-jaga.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+set -e
+
+gh_repo="libreoffice-style-karasa-jaga"
+gh_desc="Karasa Jaga LibreOffice icon themes"
+
+cat <<- EOF
+                  NNmmddddddddmm
+             Nmdhhhhhhhhhhhhhhhhhhdm
+          Nmdhhhhhysssooooosssyyhhhhhdm
+        Ndhhhhysooooooooooooooooossyhhhhd
+      Mmhhhhsooooooooooooooooooooooosyhhhhm
+    MNdhhhsooooooooooooooooooooooooooooyhhhdN
+   MNhhhhoooooo+////////////////////////ohhhhN
+  MNhhhhoooooo+`                       -oshhhhN
+  Mdhhhoooooo/                        :oooshhhdM
+  Nhhhyooooo:          .:::::::::::::/oooooyhhhN
+  mhhhsoooo:```  `    -ooooooooooooooooooooshhhm
+  mhhhsoooooooo/..-`  `:oooooooooooooooooooshhhm
+  mhhhsoooooooooo/`--`  `/oooooooooooooooooshhhm
+  Nhhhyoooooooooooo/`--   ./oooooooooooooooyhhhN
+   dhhhsooooooooooooo:`--   .++oooooooooooshhhd
+   Nhhhhsoooo:                /ooooooooooshhhh
+    Nhhhhsoo-               `+ooooooooooshhhhN
+     Ndhhhyo////////////////+ooooooooooyhhhdN
+      Mmhhhhysoooooooooooooooooooooosyhhhhm
+        NdhhhhyssoooooooooooooooossyhhhhdN
+           mdhhhhhyyssssssssssyyhhhhhdmN
+             NmdhhhhhhhhhhhhhhhhhhdmN
+                   mmddddddddmmNN
+
+
+  $gh_desc
+  https://github.com/rizmut/$gh_repo
+EOF
+
+temp_dir="$(mktemp -d)"
+
+echo "=> Getting the latest version from GitHub ..."
+curl -O "/tmp/$gh_repo.tar.gz" \
+  https://github.com/rizmut/$gh_repo/archive/master.tar.gz
+echo "=> Unpacking archive ..."
+tar -xzf "/tmp/$gh_repo.tar.gz" -C "$temp_dir"
+echo "=> Deleting old $gh_desc ..."
+sudo rm -f "/usr/share/libreoffice/share/config/images_karasa_Jaga.zip"
+echo "=> Installing ..."
+sudo mkdir -p "/usr/share/libreoffice/share/config"
+sudo cp -R \
+  "$temp_dir/$gh_repo-master/build/images_karasa_Jaga.zip" \
+  "/usr/share/libreoffice/share/config"
+for dir in \
+  /usr/lib64/libreoffice/share/config \
+  /usr/lib/libreoffice/share/config \
+  /usr/local/lib/libreoffice/share/config \
+  /opt/libreoffice*/share/config; do
+  [ -d "$dir" ] || continue
+  sudo ln -sf "/usr/share/libreoffice/share/config/images_karasa_Jaga.zip" "$dir"
+done
+echo "=> Clearing cache ..."
+rm -rf "/tmp/$gh_repo.tar.gz" "$temp_dir"
+echo "=> Done!"
