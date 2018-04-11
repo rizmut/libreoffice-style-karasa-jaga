4b54c7fc1d0b0bdd85dc9c734dd265589e8bc0db
Rizal Muttaqin <mrizal@bajau.com>
2018-04-11 11:46:11 +0700

    Add support for the icon as an extension

---
+++ create-extension.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+gh_repo="libreoffice-style-karasa-jaga"
+gh_desc="Karasa Jaga LibreOffice icon themes"
+
+cat <<- EOF
+
+  $gh_desc
+  https://github.com/rizmut/$gh_repo
+  
+  
+EOF
+echo "=>Remove old extension"
+rm "build/Karasa_Jaga-IconSet.oxt"
+echo "=>Create the new one"
+cd "build"
+cp "images_karasa_Jaga.zip" \
+   "Karasa_Jaga-IconSet/iconsets"
+cd "./Karasa_Jaga-IconSet"
+zip -r -D Karasa_Jaga-IconSet.oxt *
+mv "Karasa_Jaga-IconSet.oxt" \
+   "./.."
